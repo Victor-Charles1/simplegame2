8b0497c0631a80aa338d40a4ebde906d193f0d29
Victor-Charles1 <91386255+Victor-Charles1@users.noreply.github.com>
2025-04-29 17:54:03 -0400

    completed Gameboard object IIFE

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const gameBoard = (function(){
+    let board = Array(9).fill('')
+
+    const getBoard = function (){
+    return board;
+    }
+
+    const updateBoard = function (index, symbol){
+        if (index >= 0 && index < 9 && board[index] === '') {
+            board[index] = symbol;
+            return true;
+        }
+        return false;
+    }
+
+    const clearBoard = function (){
+         board = Array(9).fill('')
+    }
+
+    const isCellEmpty = function(index){
+        //
+        if(boardGrid[index]===''){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    const isBoardFull = function(){
+        return !board.includes('');
+     
+    }
+    return {
+        getBoard,
+        updateBoard,
+        clearBoard,
+        isBoardFull,
+        isCellEmpty
+    };
+
+
+})();
